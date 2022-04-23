258ba764edf14dcbba04ab1d899f74f1ea8fcc30
liang liang <lsqd3v@gmail.com>
2022-04-23 23:38:26 +0200

    update nft metadata desceription

--- scripts/2_mint_NFT.js
+++ scripts/2_mint_NFT.js
@@ -1,35 +1,10 @@
-const http = require("https");
-
-const options = {
-  method: "POST",
-  hostname: "api-eu1.tatum.io",
-  port: null,
-  path: "/v3/nft/mint",
-  headers: {
-    "content-type": "application/json",
-    "x-testnet-type": "SOME_STRING_VALUE",
-    "x-api-key": "REPLACE_KEY_VALUE",
-  },
-};
-
-const req = http.request(options, function (res) {
-  const chunks = [];
-
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function () {
-    const body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
+import { Currency, mintNFTWithUri } from "@tatumio/tatum";
+const transactionHash = await mintNFTWithUri(false, {
+  to: "0x687422eEA2cB73B5d3e242bA5456b782919AFc85",
+  url: "ipfs://bafybeidi7xixphrxar6humruz4mn6ul7nzmres7j4triakpfabiezll4ti/metadata.json",
+  tokenId: "100000",
+  chain: Currency.MATIC,
+  contractAddress: "0x687422eEA2cB73B5d3e242bA5456b782919AFc85",
+  fromPrivateKey:
+    "0x05e150c73f1920ec14caa1e0b6aa09940899678051a78542840c2668ce5080c2",
 });
-
-req.write(
-  JSON.stringify({
-    chain: "MUMBAI",
-    to: "0x687422eEA2cB73B5d3e242bA5456b782919AFc85", //read address
-    url: "https://my_token_data.com", //metadata for the token
-  })
-);
-req.end();
