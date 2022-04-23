2eb2e8f6fe4f03d14901e0132a566e6f21459f8f
Connor <wardconnor17@gmail.com>
2022-04-23 19:09:17 +0100

    add fake community page

--- web/pages/mint.tsx
+++ web/pages/mint.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Layout from "layout";
 import MintPage from "components/Mint/index";
+import Head from "next/Head";
 
 export default function Mint() {
   return (
     <Layout>
+      <Head>
+        <title>Mint</title>
+      </Head>
       <MintPage />
     </Layout>
   );
