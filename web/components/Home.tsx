cee18c39140b4f2bfabac62475e8785f5857d816
Connor <wardconnor17@gmail.com>
2022-04-23 09:14:52 +0100

    move to monorepo structure

---
+++ web/components/Home.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <svg
+        width="300"
+        height="300"
+        viewBox="0 0 300 300"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="150" cy="150" r="150" fill="url(#paint0_linear_217_2)" />
+        <defs>
+          <linearGradient
+            id="paint0_linear_217_2"
+            x1="18"
+            y1="86.5"
+            x2="267"
+            y2="258.5"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#FF00F5" />
+            <stop offset="0.499884" stopColor="#0027F5" />
+            <stop offset="1" stopColor="#6DFEFE" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  );
+}
