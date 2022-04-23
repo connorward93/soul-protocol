b711e30fa02766aa88bea9be305c362ca7af7854
liang liang <lsqd3v@gmail.com>
2022-04-23 23:12:50 +0200

    add contract structures, and nft metadata

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ada175ede139b355"; }
+/******/ 		__webpack_require__.h = function() { return "e9de6fbd96261006"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
