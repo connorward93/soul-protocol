6eff554987c0c795623ec777237cb3f0c79541ca
liang liang <lsqd3v@gmail.com>
2022-04-24 03:03:59 +0200

    edit soul.sol and mint script

--- scripts/2_mint_NFT.js
+++ scripts/2_mint_NFT.js
@@ -1,10 +1,57 @@
-import { Currency, mintNFTWithUri } from "@tatumio/tatum";
+import { Currency, mintNFTWithUri, ipfsUpload } from "@tatumio/tatum";
+
+//import img SVG
+
+/**
+ * Upload file to the IPFS storage.
+ * @param file - data buffer of the file. Content Type: multipart/form-data
+ * @returns ipfsHash - IPFS hash of the file
+ */
+const imgHash = await ipfsUpload(SVG, "Soul Image");
+
+const metadata = {
+  name: "Soul",
+  desicription:
+    'Your Soul NFT represents your most authentic "true self" on chain. An interoperable "you" that bridges pysical and vitrual world. A build block for a better web. Connect yourself severeign Soul to the World, and watch it evolves along side you.',
+  image: imgHash.ipfshash,
+  attributes: [
+    {
+      R: r, //hex
+      G: g,
+      B: b,
+    },
+    {
+      display_type: "boost_percentage",
+      trait_type: "Active",
+      value: 10, //pull from oracle
+    },
+    {
+      display_type: "boost_percentage",
+      trait_type: "Passive",
+      value: 10,
+    },
+    {
+      display_type: "boost_percentage",
+      trait_type: "Postive",
+      value: 10,
+    },
+    {
+      display_type: "boost_percentage",
+      trait_type: "Negative",
+      value: 10,
+    },
+    {
+      display_type: "date",
+      trait_type: "collection date",
+      value: 1546360800,
+    },
+  ],
+};
+
+const nftHash = await ipfsUpload(metadata, "tokenMetadata");
+
 const transactionHash = await mintNFTWithUri(false, {
   to: "0x687422eEA2cB73B5d3e242bA5456b782919AFc85",
-  url: "ipfs://bafybeidi7xixphrxar6humruz4mn6ul7nzmres7j4triakpfabiezll4ti/metadata.json",
-  tokenId: "100000",
-  chain: Currency.MATIC,
-  contractAddress: "0x687422eEA2cB73B5d3e242bA5456b782919AFc85",
-  fromPrivateKey:
-    "0x05e150c73f1920ec14caa1e0b6aa09940899678051a78542840c2668ce5080c2",
+  url: nftHash,
+  chain: Currency.MUMBAI,
 });
