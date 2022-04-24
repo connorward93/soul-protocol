7e36e451f32e2784e5dd3a87d3e5a4cc400fbc0e
liang liang <lsqd3v@gmail.com>
2022-04-24 07:26:28 +0200

    almost add the moodring api

--- web/components/Mint/Questions.tsx
+++ web/components/Mint/Questions.tsx
@@ -1,6 +1,67 @@
 import React from "react";
 import classes from "./mint.module.scss";
+import axios from 'axios';
+require('dotenv').config();
 
-export default function Questions() {
-  return <div className={classes.questions}>Questions</div>;
+
+type State = "loading" | "noQuestions" | "done";
+type QuestionsProps = { readonly state?: State};
+type GetQuestionsRes = {
+  data:[];
+};
+
+
+async function getQuestions() {
+  try {
+    // 👇️ const data: GetQuestions
+    const { data, status } = await axios.get<GetQuestionsRes>(
+      'https://moodring.p.rapidapi.com/quizzes/1',
+      {
+        headers: {
+          Accept: 'application/json',
+          'X-RapidAPI-Host': 'moodring.p.rapidapi.com',
+          'X-RapidAPI-Key': `${process.env.RAPID_API}`
+        }
+      },
+    );
+
+    console.log(JSON.stringify(data, null, 4));
+
+    // 👇️ "response status is: 200"
+    console.log('response status is: ', status);
+
+    console.log(data);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
 }
+
+
+getQuestions();
+
+
+
+
+
+export const Questions: React.FC<QuestionsProps> = ({ state = "loading"}) =>  {
+  const State = {
+    loading: () => {/* Your loading code here */},
+    noQuestions: () => {/* Your failed code here */},
+    done: () => {/* Your done code here */}
+
+    } as Record<State, Fc>[State];
+  }
+  return (
+    <div className={classes.questions}>
+      <State />
+    </div>
+  )
+  };
+
+
